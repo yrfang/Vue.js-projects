a4555e0a299c7a90ff499839f454ef3eeece03f4
yrfang <fyr1116@gmail.com>
2017-06-15 10:36:47 +0800

    'icon-css'

--- 08 - wtichable-grid/switchable-grid.js
+++ 08 - wtichable-grid/switchable-grid.js
@@ -5,52 +5,28 @@ var demo = new Vue({
         layout: 'list',
 
         articles: [{
-            "title": "What You Need To Know About CSS Variables",
-            "url": "https://tutorialzine.com/2016/03/what-you-need-to-know-about-css-variables/",
-            "image": {
-                "large": "https://tutorialzine.com/media/2016/03/css-variables.jpg",
-                "small": "https://tutorialzine.com/media/2016/03/css-variables.jpg"
-            }
+            "title": "Superhero 做自己人生裡的超級英雄",
+            "image": "https://unsplash.it/400/300?image=1051"
         },
         {
-            "title": "Freebie: 4 Great Looking Pricing Tables",
-            "url": "https://tutorialzine.com/2016/02/freebie-4-great-looking-pricing-tables/",
-            "image": {
-                "large": "https://tutorialzine.com/media/2016/02/great-looking-pricing-tables.jpg",
-                "small": "https://tutorialzine.com/media/2016/02/great-looking-pricing-tables.jpg"
-            }
+            "title": "Free 偶爾迷糊，換得自在",
+            "image": "https://unsplash.it/400/300?image=1042"
         },
         {
-            "title": "20 Interesting JavaScript and CSS Libraries for February 2016",
-            "url": "https://tutorialzine.com/2016/02/20-interesting-javascript-and-css-libraries-for-february-2016/",
-            "image": {
-                "large": "https://tutorialzine.com/media/2016/02/interesting-resources-february.jpg",
-                "small": "https://tutorialzine.com/media/2016/02/interesting-resources-february.jpg"
-            }
+            "title": "Small 世界多大，我們多渺小；看遠一點，就不計較了",
+            "image": "https://unsplash.it/400/300?image=1049"
         },
         {
-            "title": "Quick Tip: The Easiest Way To Make Responsive Headers",
-            "url": "https://tutorialzine.com/2016/02/quick-tip-easiest-way-to-make-responsive-headers/",
-            "image": {
-                "large": "https://tutorialzine.com/media/2016/02/quick-tip-responsive-headers.png",
-                "small": "https://tutorialzine.com/media/2016/02/quick-tip-responsive-headers.png"
-            }
+            "title": "World 世界是一本書，不旅行的人只讀了一頁",
+            "image": "https://unsplash.it/400/300?image=1015"
         },
         {
-            "title": "Learn SQL In 20 Minutes",
-            "url": "https://tutorialzine.com/2016/01/learn-sql-in-20-minutes/",
-            "image": {
-                "large": "https://tutorialzine.com/media/2016/01/learn-sql-20-minutes.png",
-                "small": "https://tutorialzine.com/media/2016/01/learn-sql-20-minutes.png"
-            }
+            "title": "Travel 旅行是唯一讓你花了錢卻變得更富有的東西",
+            "image": "https://unsplash.it/400/300?image=988"
         },
         {
-            "title": "Creating Your First Desktop App With HTML, JS and Electron",
-            "url": "https://tutorialzine.com/2015/12/creating-your-first-desktop-app-with-html-js-and-electron/",
-            "image": {
-                "large": "https://tutorialzine.com/media/2015/12/creating-your-first-desktop-app-with-electron.png",
-                "small": "https://tutorialzine.com/media/2015/12/creating-your-first-desktop-app-with-electron.png"
-            }
+            "title": "Run 訓練最痛苦的部分，就是一開始穿上鞋子的那一刻",
+            "image": "https://images.unsplash.com/photo-1470850364367-9a570ba95492?dpr=1.100000023841858&auto=compress,format&fit=crop&w=767&h=573&q=80&cs=tinysrgb&crop=&bg="
         }]
 
     }
