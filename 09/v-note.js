4b5ca906e25314dad4294824bbec60fd85367bca
yrfang <fyr1116@gmail.com>
2017-06-16 17:22:09 +0800

    'css-display-and-v-model'

--- 09/v-note.js
+++ 09/v-note.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   var test_notes = [
     {
-      "title": "今天嘗試自己做今天嘗試自己，今天嘗試自己1111，11112，22222222",
+      "title": "今天嘗試自己做",
       "text": "類似todolist"
     },
     {
-      "title": "title2...title2...抬頭吐抬頭吐抬頭吐",
-      "text": "continue..."
+      "title": "君不見黃河之水天上來,君不見黃河之水天上來",
+      "text": "朝如青絲暮成雪？天生我材必有用"
     },
     {
       "title": "title3...",
-      "text": "為什麼寫了很多字，可是好像還是沒有自動換行，該如何做到？"
+      "text": "君不見黃河之水天上來，奔流到海不復回？，君不見高堂明鏡悲白髮，朝如青絲暮成雪？天生我材必有用，千金散盡還復來。"
     },
     {
       "title": "title4...",
@@ -23,6 +23,25 @@ $(document).ready(function() {
     el: "#app",
     data: {
       notes: test_notes,
+      show_note: false,
+    },
+    methods: {
+      add_new_note: function() {
+        this.notes.push({
+          "title": "New note",
+          "text": "Your new note"
+        });
+      },
+      delete_note: function(id) {
+        this.notes.splice(id,1);
+      },
+      toggle_edit_note: function() {
+
+        this.show_note = !this.show_note;
+      },
+      hide_edit_note: function() {
+        this.show_note = false;
+      }
     }
   });
 
