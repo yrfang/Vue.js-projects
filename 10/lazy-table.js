383e8a3eccf33b77dc78fe32e7473a63d0d30109
yrfang <fyr1116@gmail.com>
2017-07-02 20:46:57 +0800

    vue-lazy-table no component

---
+++ 10/lazy-table.js
@@ -0,0 +1,38 @@
+var idData = "https://gist.githubusercontent.com/arthurSena/bd98c43ca6b5e1c642b4/raw/789ba104e2692bc63d4442ef730c7f3e9755b76d/data.json";
+
+Vue.component("lazytable", {
+  template: "#table_template",
+  props: ["table_data", "rows","id", "columns"],
+  data() {
+    return {
+      search_key: ""
+    }
+  },
+  computed: {
+    keys() {
+      let rows = this.table_data;
+      let row_keys = Object.keys(rows)
+                           .reduce((prep, cur) => prep.concat(cur),[])
+                           .filter((obj,index,arr) => arr.indexOf(obj)==index)
+      return row_keys;
+    }
+  }
+});
+
+var vm = new Vue({
+  el: "#app",
+  data: {
+    idData: [],
+    columns: ['id', 'First name', 'Last name', 'Email', 'Country'],
+  },
+  mounted: function() {
+    var vobj=this;
+    $.ajax({
+      url: idData,
+      success: function(res) {
+        // console.log(vobj.idData = JSON.parse(res));
+        vobj.idData = JSON.parse(res);
+      }
+    });
+  }
+});
